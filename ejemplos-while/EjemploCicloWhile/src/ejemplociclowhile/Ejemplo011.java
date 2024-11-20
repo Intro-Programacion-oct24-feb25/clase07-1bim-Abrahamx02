a02c26005b7660b060beca48b9dfba0f7238cb19
abrahamx02 <georssj@gmail.com>
2024-11-20 11:40:59 -0500

    1cambio

---
+++ ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo011.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejemplociclowhile;
+
+/**
+ *
+ * @author reroes
+ */
+public class Ejemplo011 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        int contador = 1;
+        int limit = 13;
+
+        while (contador <= limit){
+             System.out.printf("%d\n", contador);
+             contador = contador + 1;
+
+        }
+                
+        
+    }
+    
+}

3c90d0c166a14f5270efda7dfe07c5650eab4e34
abrahamx02 <georssj@gmail.com>
2024-11-21 11:31:13 -0500

    cambios2

--- ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo101.java
+++ ejemplos-while/EjemploCicloWhile/src/ejemplociclowhile/Ejemplo101.java
@@ -20,16 +20,28 @@ public class Ejemplo101 {
         // TODO code application logic here
         Scanner entrada = new Scanner(System.in);
         entrada.useLocale(Locale.US);
-        int limite_tabla = 12;
+        int limite_tabla;
         int contador = 1;
         int operacion;
         int tabla;
+        
+        System.out.println("Ingrese el limite de la tabla");
+        limite_tabla = entrada.nextInt();
         
         System.out.println("Ingrese el número de tabla a generar");
         tabla = entrada.nextInt();
         
         String cadena = ""; // cadena acumulador
         cadena = String.format("%sTabla de multiplicar\n", cadena);
+        
+        if (limite_tabla > 0 ){
+            limite_tabla = limite_tabla;
+            
+        }else{
+            if (limite_tabla <=0){
+                limite_tabla = 12;
+                }
+        }
         
         while (contador <= limite_tabla){
             operacion = tabla * contador;
